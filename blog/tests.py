660ed36b15645216990c4ed5707350ec7bb87ef1
gaeun0915 <dlrkdms8233@naver.com>
2022-08-16 09:34:03 +0900

    네비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -6,17 +6,32 @@ class TestView(TestCase) :
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog',navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a',text="스마트 부산")
+        self.assertEqual(log_btn.attrs['href'],'/')
+
+        homebtn = navbar.find('a', text="Home")
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text="Blog")
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_btn = navbar.find('a', text="About Me")
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
         soup = BeautifulSoup(response.content,'html.parser')
+
         self.assertEqual(soup.title.text, 'Blog')
 
-        navbar = soup.nav
-
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About Me', navbar.text)
 
         self.assertEqual(Post.objects.count(),0)
 
@@ -54,9 +69,7 @@ class TestView(TestCase) :
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content,'html.parser')
 
-        navbar = soup.nav
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         self.assertIn(post_001.title, soup.title.text)
 
